f55a5f757110daae3b8788d55a6fb52869c4ccd2
George <chartounygeorge@gmail.com>
2023-06-04 21:47:31 +0300

    search function

--- lib/main.dart
+++ lib/main.dart
@@ -38,9 +38,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // to make http requests
-  // http.Client client = http.Client();
+  // variable gets set when the user types in the search bar, otherwise empty string
+  String searchWord = '';
+
+  // filtered list books by searchWord
+  List<Book> filteredBooks = [];
+
+  // list of all books
   List<Book> books = [];
+
 // whenever this part gets rendered, this function will be called
   @override
   void initState() {
@@ -56,8 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
     var responseData = json.decode(response.body);
 
-// List to store books data
-    List<Book> books = [];
     //looing over data and adding them to the list declared
     for (var singleBook in responseData) {
       Book book = Book(
@@ -69,85 +73,116 @@ class _MyHomePageState extends State<MyHomePage> {
           additional_details: singleBook["additional_details"]);
       books.add(book);
     }
-    return books;
+    // return books;
+    setState(() {
+      filteredBooks = books;
+    });
+    return filteredBooks;
   }
 
-  void _addBook() {}
-
+  // delete function tha calls the delete http request with the appropriate id
   void deleteBook(int id) {
     http.delete(Uri.parse("$url/book/$id/delete"));
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => const MyApp()));
   }
+// Rest of the code...
 
   @override
   Widget build(BuildContext context) {
+    filteredBooks = searchWord.isEmpty
+        ? books
+        : books.where((book) {
+            final lowerCaseSearchWord = searchWord.toLowerCase();
+            return book.title!.toLowerCase().contains(lowerCaseSearchWord) ||
+                book.author!.toLowerCase().contains(lowerCaseSearchWord) ||
+                book.category!.toLowerCase().contains(lowerCaseSearchWord) ||
+                book.pub_year!.contains(searchWord);
+          }).toList();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Container(
-        child: FutureBuilder(
-          future: fetchBooks(),
-          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
-              return Container(
-                child: Center(child: CircularProgressIndicator()),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (ctx, index) => ListTile(
-                  title: Text(
-                    snapshot.data[index].title,
-                    style: const TextStyle(fontSize: 25, color: Colors.grey),
-                  ),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => UpdateBookPage(
-                                title: snapshot.data[index].title,
-                                author: snapshot.data[index].author,
-                                category: snapshot.data[index].category,
-                                pub_year: snapshot.data[index].pub_year,
-                                additional_details:
-                                    snapshot.data[index].additional_details,
-                                id: snapshot.data[index].id,
-                              )),
-                    );
-                  },
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () => deleteBook(snapshot.data[index].id),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'By: ${snapshot.data[index].author}',
-                        style: const TextStyle(
-                            fontSize: 17, fontStyle: FontStyle.italic),
-                      ),
-                      Text(
-                        'Publication Year: ${snapshot.data[index].pub_year}',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Category: ${snapshot.data[index].category}',
-                        style: const TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                      Text(
-                        'Additional Details: ${snapshot.data[index].additional_details}',
-                        style: const TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ],
-                  ),
-                  contentPadding: const EdgeInsets.only(bottom: 20.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    searchWord = value;
+                  });
+                },
+                decoration: const InputDecoration(
+                  label: Text('Search'),
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
                 ),
-              );
-            }
-          },
+              ),
+            ),
+            Expanded(
+              child: filteredBooks.isEmpty
+                  ? const Center(child: Text('No books found'))
+                  : ListView.builder(
+                      itemCount: filteredBooks.length,
+                      itemBuilder: (ctx, index) => ListTile(
+                        title: Text(
+                          filteredBooks[index].title!.toUpperCase(),
+                          style:
+                              const TextStyle(fontSize: 25, color: Colors.grey),
+                        ),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => UpdateBookPage(
+                                title: filteredBooks[index].title!,
+                                author: filteredBooks[index].author!,
+                                category: filteredBooks[index].category!,
+                                pub_year: filteredBooks[index].pub_year!,
+                                additional_details:
+                                    filteredBooks[index].additional_details,
+                                id: filteredBooks[index].id!,
+                              ),
+                            ),
+                          );
+                        },
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => deleteBook(filteredBooks[index].id!),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'By: ${filteredBooks[index].author}',
+                              style: const TextStyle(
+                                  fontSize: 17, fontStyle: FontStyle.italic),
+                            ),
+                            Text(
+                              'Publication Year: ${filteredBooks[index].pub_year}',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'Category: ${filteredBooks[index].category}',
+                              style:
+                                  const TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                            Text(
+                              'Additional Details: ${filteredBooks[index].additional_details}',
+                              style:
+                                  const TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ],
+                        ),
+                        contentPadding: const EdgeInsets.only(bottom: 20.0),
+                      ),
+                    ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -156,7 +191,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         tooltip: 'AddBook',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
